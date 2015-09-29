7fcce7128b20c7ae6c7ecf85470c022bb505206e
adammcc <ajm845@gmail.com>
2015-09-28 23:09:53 -0400

    A few ui and style changes changes
    - Click photos to enter show page
    - No more expanding image blurbs
    - Font family changes
    - Other minor style adjustments

--- components/BkBox.jsx
+++ components/BkBox.jsx
@@ -4,28 +4,21 @@ var BkBox = React.createClass({
       selected: false
     };
   },
-  select: function (event) {
-    this.setState({
-      selected: !this.state.selected
-    });
+  select: function () {
+    this.setState({ selected: true });
+  },
+  deselect: function () {
+    this.setState({ selected: false });
   },
   render: function () {
     return (
-      <div className={ this.state.selected ? "bk-box -selected" : "bk-box" } onClick={ this.select }>
+      <div className={ this.state.selected ? "bk-box -selected" : "bk-box" } onClick={ this.props.showPage.bind(null, this.props.pics) } onMouseEnter={ this.select } onMouseLeave={ this.deselect }>
         <div className="bk-box__img-container">
           <img src={this.props.src} alt={this.props.alt} height="auto" width="100%" />
-          <i className="fa fa-plus" onClick={ this.props.showPage.bind(null, this.props.pics) }></i>
+          <i className="fa fa-chevron-right"></i>
         </div>
-        <h4>{ this.props.title }</h4>
-        { this.state.selected ? (
-          <p>
-            { this.props.children }
-          </p>
-        ) : (
-          <p>
-            { this.props.children.substring(0,50) + "..." }
-          </p>
-        )}
+        <h4 className="bk-box__title">{ this.props.title }</h4>
+        <p>{ this.props.children }</p>
       </div>
     );
   }
