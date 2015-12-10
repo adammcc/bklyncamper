4e6e6abddce8822eda8aebd02b5d327b2bde1e08
adammcc <ajm845@gmail.com>
2015-12-09 23:49:32 -0500

    Add filters to articles page

--- components/BodyContent.jsx
+++ components/BodyContent.jsx
@@ -12,23 +12,39 @@ var BodyContent = React.createClass({
     }
     return out;
   },
+  getArticles: function () {
+    var articleTitles = Object.keys(info)
+    if ( this.props.filter == 'everything' ) {
+      var num = Math.ceil(articleTitles.length / 3);
+      return this.split(articleTitles, num);
+    } else {
+      var filteredTitles = []
+      var filter = this.props.filter
+      $.each( articleTitles, function( i, title ) {
+        if ( info[title]['filter'] == filter ) {
+          filteredTitles.push(title);
+        };
+      });
+      var num = Math.ceil(filteredTitles.length / 3);
+      return this.split(filteredTitles, num);
+    };
+  },
   render: function () {
-    var infoArray = []
-    for (var key in info) { infoArray.push([key, info[key]]) }
-    var infos = this.split(infoArray, 3)
+    var titles_in_threes = this.getArticles()
+    var filter = this.props.filter
     return (
       <div>
         <a href="#">
           <BkMainHeader />
         </a>
 
-        <BkFilters/>
+        <BkFilters filter={filter}/>
 
-        <BkRow articles={['finger_lakes', 'mohonk', 'hudson_winter_walk']} />
-        <BkRow articles={['new_haven', 'hoot', 'bear_mountain']} />
-        <BkRow articles={['hudson_winter_walk', 'nyack', 'newport']} />
-        <BkRow articles={['newport', 'storm_king', 'scotland']} />
-        <BkRow articles={['marthas_vineyard', 'saugerties', 'storm_king']} />
+        { titles_in_threes.map(function (three_titles) {
+          return (
+            <BkRow threeTitles={three_titles} />
+          );
+        })}
       </div>
     );
   }
